9fc74757941aa224cdae31f8c21bb6f1d61f5b57
MD Amirul Islam <50146502+MDAmir159@users.noreply.github.com>
2021-09-03 15:32:17 +0600

    Update index.js

--- server/index.js
+++ server/index.js
@@ -7,10 +7,23 @@ app.use(express.urlencoded({extended : true}));
 app.use(cors())
 app.use(express.json());
 
-app.use(function(req,res,next){
-     res.setHeader("Access-Control-Allow-Origin", "https://cracked-project.herokuapp.com");
-     res.setHeader("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT");
-     next();
+app.use(function (req, res, next) {
+
+    // Website you wish to allow to connect
+    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:8888');
+
+    // Request methods you wish to allow
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+    // Request headers you wish to allow
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+    // Set to true if you need the website to include cookies in the requests sent
+    // to the API (e.g. in case you use sessions)
+    res.setHeader('Access-Control-Allow-Credentials', true);
+
+    // Pass to next layer of middleware
+    next();
 });
 
 const logInRouter = require('./view/logIn');
