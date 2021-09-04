021dbf920bf99950a215d1c9706d3585cbc75ffa
MD Amirul Islam <50146502+MDAmir159@users.noreply.github.com>
2021-09-04 21:39:14 +0600

    Update index.js

--- server/index.js
+++ server/index.js
@@ -7,27 +7,27 @@ app.use(express.urlencoded({extended : true}));
 app.use(cors())
 app.use(express.json());
 
-app.options('*', cors()) // include before other routes
+//app.options('*', cors()) // include before other routes
 
-app.use(function (req, res, next) {
+//app.use(function (req, res, next) {
     
-    res.json({message : "Inside"});
+    //res.json({message : "Inside"});
     // Website you wish to allow to connect
-    res.setHeader('Access-Control-Allow-Origin', 'https://fast-meadow-49435.herokuapp.com/');
+    //res.setHeader('Access-Control-Allow-Origin', 'https://fast-meadow-49435.herokuapp.com/');
 
     // Request methods you wish to allow
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+    //res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
 
     // Request headers you wish to allow
-    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+    //res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
 
     // Set to true if you need the website to include cookies in the requests sent
     // to the API (e.g. in case you use sessions)
-    res.setHeader('Access-Control-Allow-Credentials', true);
+    //res.setHeader('Access-Control-Allow-Credentials', true);
 
     // Pass to next layer of middleware
-    next();
-});
+    //next();
+//});
 
 const logInRouter = require('./view/logIn');
 app.use('/logIn', logInRouter);
